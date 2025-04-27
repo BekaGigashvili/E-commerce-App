1a0b526dcbccf63555adafafcdf00fc677eb30d9
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-27 04:23:18 +0400

    creating confirmatio token repository

---
+++ src/main/java/com/javaprojects/ecommerce/repository/ConfirmationTokenRepository.java
@@ -0,0 +1,12 @@
+package com.javaprojects.ecommerce.repository;
+
+import com.javaprojects.ecommerce.model.ConfirmationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+    Optional<ConfirmationToken> findByToken(String token);
+}
