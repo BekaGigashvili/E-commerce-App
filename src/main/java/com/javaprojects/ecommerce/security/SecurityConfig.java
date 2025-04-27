9740a10a4ac3431467c40c2303b8c3a60bc56094
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-27 23:12:44 +0400

    security configuration for authenticated requests

--- src/main/java/com/javaprojects/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/javaprojects/ecommerce/security/SecurityConfig.java
@@ -1,17 +1,21 @@
 package com.javaprojects.ecommerce.security;
 
+import com.javaprojects.ecommerce.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,11 +26,14 @@ public class SecurityConfig {
                                 .requestMatchers("/user/auth/**", "/user/verify/**").permitAll()
                                 .anyRequest().authenticated()
                         )
+                        .sessionManagement(
+                                session -> session
+                                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        )
+                        .addFilterBefore(
+                                jwtAuthenticationFilter,
+                                UsernamePasswordAuthenticationFilter.class
+                        )
                         .build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
