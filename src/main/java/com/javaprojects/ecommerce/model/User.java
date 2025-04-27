819e07ab9041897809d1b36429621e4e081cb152
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-27 04:22:07 +0400

    creating user repository

---
+++ src/main/java/com/javaprojects/ecommerce/model/User.java
@@ -0,0 +1,32 @@
+package com.javaprojects.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    private boolean enabled;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.PERSIST)
+    private ConfirmationToken token;
+}
