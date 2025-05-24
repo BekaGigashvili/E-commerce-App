fb9e84c40ec85e87be9f20f4cd1c6d361de197cc
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-05-25 02:11:56 +0400

    adding product fetching by some constraints functionality

--- src/main/java/com/javaprojects/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/javaprojects/ecommerce/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.javaprojects.ecommerce.repository;
 
+import com.javaprojects.ecommerce.model.Category;
 import com.javaprojects.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategory(Category category);
+    List<Product> findByName(String name);
 }
--- src/main/java/com/javaprojects/ecommerce/service/ProductService.java
+++ src/main/java/com/javaprojects/ecommerce/service/ProductService.java
@@ -1,15 +1,44 @@
 package com.javaprojects.ecommerce.service;
 
+import com.javaprojects.ecommerce.model.Category;
 import com.javaprojects.ecommerce.model.Product;
 import com.javaprojects.ecommerce.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+
+    @Transactional
+    public List<Product> findByNameAndCategory(String name, Category category){
+        List<Product> byName = findByName(name);
+        List<Product> byCategory = findByCategory(category);
+        List<Product> result = new ArrayList<>();
+        for (Product product : byName) {
+            if(byCategory.contains(product)){
+                result.add(product);
+            }
+        }
+        return result;
+    }
+
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+
+    public List<Product> findByName(String name){
+        return productRepository.findByName(name);
+    }
+
+    public List<Product> findByCategory(Category category) {
+        return productRepository.findByCategory(category);
+    }
 
     @Transactional
     public String addProduct(Product product) {
