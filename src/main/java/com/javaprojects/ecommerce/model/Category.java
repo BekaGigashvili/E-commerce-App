48d7e24b928f16b85fd10701c8f094a40257040f
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-05-24 14:00:09 +0400

    adding category

---
+++ src/main/java/com/javaprojects/ecommerce/model/Category.java
@@ -0,0 +1,16 @@
+package com.javaprojects.ecommerce.model;
+
+public enum Category {
+    ELECTRONICS,
+    CLOTHING,
+    FOOTWEAR,
+    JEWELRY,
+    HOME,
+    BEAUTY,
+    SPORTS,
+    BOOKS,
+    TOYS,
+    FOOD,
+    HEALTH,
+    FURNITURE
+}
